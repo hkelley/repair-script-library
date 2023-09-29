cea6e75f9b852a53a14be6b0fc243b2ca2b43450
hkelley <8828244+hkelley@users.noreply.github.com>
2023-09-29 18:08:20 -0400

    Update test.ps1

--- src/windows/test.ps1
+++ src/windows/test.ps1
@@ -1,7 +1,16 @@
-
 Param([Parameter(Mandatory=$false)][string]$gen='1')
-. .\src\windows\common\setup\init.ps1
 
-Write-Output "Running Script Enable-NestedHyperV $gen"
-Log-Output '$Gen'
+# Initialize script
+. .\src\windows\common\setup\init.ps1
+#. .\src\windows\common\helpers\Get-Disk-Partitions.ps1
+
+# Declare variables
+$scriptStartTime = get-date -f yyyyMMddHHmmss
+$scriptPath = split-path -path $MyInvocation.MyCommand.Path -parent
+$scriptName = (split-path -path $MyInvocation.MyCommand.Path -leaf).Split('.')[0]
+$guestHyperVVirtualMachine = Get-VM
+$guestHyperVVirtualMachineName = $guestHyperVVirtualMachine.VMName
+
+Log-Output "START: Running script $scriptName on $guestHyperVVirtualMachineName "
+
 return $STATUS_SUCCESS
