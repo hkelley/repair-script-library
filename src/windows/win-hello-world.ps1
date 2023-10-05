b98909bf65ebd1d506b26c3f90ce36351fe3a59c
Kelley <8828244+hkelley@users.noreply.github.com>
2023-10-05 19:32:20 -0400

    removed hello world

--- src/windows/win-hello-world.ps1
+++
@@ -1,5 +0,0 @@
-Param([Parameter(Mandatory=$false)][string]$hello='Hello',[Parameter(Mandatory=$false)][string]$world='World')
-. .\src\windows\common\setup\init.ps1
-Log-Output "$hello $world!"
-
-return $STATUS_SUCCESS
